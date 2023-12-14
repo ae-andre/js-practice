956b3b404be3924f1b6ead045c24bb2b554e36e1
dnddigest <85869787+dnddigest@users.noreply.github.com>
2023-12-13 22:35:39 -0700

    added new practice exercises

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+
+
+// exercise 1
 console.log("hello world!")
+
+//exercise 2
+
+// const num1 = parseInt(prompt('Enter first number '))
+// const num2 = parseInt(prompt('Enter second number '))
+
+// const sum = num1 + num2;
+
+// console.log(`the sum of ${num1} and ${num2} is ${sum}`)
+
+// exercise 3
+
+// const num = parseInt(prompt('Enter your number to check if odd or even '))
+
+// function verifyOdd() {
+//     if (num % 2 == 0) {
+//         console.log("even")
+//     } else {
+//         console.log("odd")
+//     }
+// }
+
+// verifyOdd()
+
+// exercise 4
+
+// function counter() {
+//     for (i = 0; i < 11; i++) {
+//         console.log(i)
+//     }
+// }
+
+// counter()
+
+// exercise 5
+// exercise 6
+
+// const sampleString = prompt('Enter a long nonsensical word now!')
+
+// console.log(sampleString.length)
+
+// exercise 7
+
+
